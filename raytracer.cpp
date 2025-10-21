7841576dec2dfb9c0c444a3bbd1fee9dfee143c6
Arman Uddin <armantausif64@gmail.com>
2025-10-20 22:10:53 -0700

    Finish Vec3 Class

--- raytracer.cpp
+++ raytracer.cpp
@@ -27,7 +27,7 @@ class Vec3 {
 public:
     T x, y, z;
     Vec3() : x(T(0)), y(T(0)), z(T(0)) {}
-    Vec3(T xx) : x(xx), y(yy), z(zz) {}
+    Vec3(T xx) : x(xx), y(xx), z(xx) {}
     Vec3(T xx, T yy, T zz) : x(xx), y(yy), z(zz) {}
     Vec3& normalize() {
         T nor2 = length2();
@@ -37,5 +37,24 @@ public:
         }
         return *this;
     }
+    Vec3<T> operator * (const T &f) const {return Vec3<T>(x*f, y * f, z * f); }
+    Vec3<T> operator * (const Vec3<T> &f) const {return Vec3<T>(x*f, y * f, z * f); }
+    T dot(const Vec3<t> &v) const {return x * v.x + y * v.y + z * v.z); }
+    Vec3<T> operator - (const Vec3<T> &v) const { return Vec3<T>(x - v.x, y - v.y, z - v.z); }
+    Vec3<T> operator + (const Vec3<T> &v) const { return Vec3<T>(x + v.x, y + v.y, z + v.z); }
+    Vec3<T>& operator += (const Vec3<T> &v) { x += v.x, y += v.y, z += v.z; return *this; }
+    Vec3<T>& operator *= (const Vec3<T> &v) { x *= v.x, y *= v.y, z *= v.z; return *this; }
+    Vec3<T> operator - () const { return Vec3<T>(-x, -y, -z); }
+    T length2() const { return x * x + y * y + z * z; }
+    T length() const { return sqrt(length2()); }
+    friend std::ostream & operator << (std::ostream &os, const Vec3<T> &v)
+    {
+        os << "[" << v.x << " " << v.y << " " << v.z << "]";
+        return os;
+    }
 
 };
+
+
+
+
